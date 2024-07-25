2d8b8918f36dcc2f5dc1c076a8439a857edb9a94
minhypro <85498660+minhypro@users.noreply.github.com>
2024-07-25 15:42:16 +0700

    Update App.tsx

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -19,6 +19,14 @@ function App(): JSX.Element {
         Build an Electron app with <span className="react">React</span>
         &nbsp;and <span className="ts">TypeScript</span>
       </div>
+      <div className="text">
+        App deployed to Vercel by Y Le
+      </div>
+      <div>
+        <a href="https://electron-vite-xi.vercel.app/" target="_blank" rel="noreferrer">
+            Web Version
+        </a>
+      </div>
       <p className="tip">
         Please try pressing <code>F12</code> to open the devTool
       </p>
